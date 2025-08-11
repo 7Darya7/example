57dc3616b2743dfbcfc8d7b7731ae702a227b9be
7Darya7 <lucky.go.dev@gmail.com>
2025-08-11 16:58:58 +0300

    wip

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	//"github.com/7Darya7/example/pkg/internal/secret"
+	"github.com/7Darya7/example/pkg/utils"
+)
+
+func main() {
+	fmt.Println(utils.Add(2, 3))
+	//fmt.Println(secret.SecretValue())
+}
